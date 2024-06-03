ff92322c90ac447b4bba115ad01d58d7eee6b3dd
Titus Weng <tw013@bucknell.edu>
2024-06-03 17:13:09 -0400

    update 3

--- scripts/preprocess_videos.py
+++ scripts/preprocess_videos.py
@@ -2,30 +2,27 @@ import os
 import cv2
 from scripts.extract_features import extract_features, save_features_to_csv
 
+def create_if_not_exsit(target_dir, msg):
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+        print(f"{msg}: {target_dir}")
+
+def preprocess_a_video(data_dir, processed_dir, lang, output_csv):
+    lang_dir = os.path.join(data_dir, lang)
+    processed_lang_dir = os.path.join(processed_dir, lang)
+    create_if_not_exsit(processed_lang_dir, "Created processed language directory")
+    for video_file in os.listdir(lang_dir):
+        if video_file.endswith('.mp4'):
+            processed_label_dir = os.path.join(processed_lang_dir, video_file.split('.')[0])
+            create_if_not_exsit(processed_label_dir, "Created processed label directory")
+            video_path = os.path.join(lang_dir, video_file)
+            print(f"Processing video: {video_path}")
+            process_video(video_path, processed_label_dir, output_csv)
+
 def preprocess_videos(data_dir, processed_dir, output_csv):
-    if not os.path.exists(processed_dir):
-        os.makedirs(processed_dir)
-        print(f"Created processed directory: {processed_dir}")
-
+    create_if_not_exsit(processed_dir, "Created processed directory")
     for lang in ['asl', 'csl']:
-        lang_dir = os.path.join(data_dir, lang)
-        processed_lang_dir = os.path.join(processed_dir, lang)
-        if not os.path.exists(processed_lang_dir):
-            os.makedirs(processed_lang_dir)
-            print(f"Created processed language directory: {processed_lang_dir}")
-
-        for label in os.listdir(lang_dir):
-            label_dir = os.path.join(lang_dir, label)
-            processed_label_dir = os.path.join(processed_lang_dir, label)
-            if os.path.isdir(label_dir):
-                if not os.path.exists(processed_label_dir):
-                    os.makedirs(processed_label_dir)
-                    print(f"Created processed label directory: {processed_label_dir}")
-                for video_file in os.listdir(label_dir):
-                    if video_file.endswith('.mp4'):
-                        video_path = os.path.join(label_dir, video_file)
-                        print(f"Processing video: {video_path}")
-                        process_video(video_path, processed_label_dir, output_csv)
+        preprocess_a_video(data_dir, processed_dir, lang, output_csv)
 
 def process_video(video_path, output_dir, output_csv):
     print(f"Processing video file: {video_path}")
